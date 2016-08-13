7e02a42d3d15f00066ae7d98bb7647c56d4409bf
Ogg (Valentin Padurean) <valentin.padurean@bitsoftware.ro>
2016-08-13 13:41:36 +0300

    Add jquery

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 // On document ready handler
-// !!!IMPORTANT:
-//   '$' alias for 'jQuery' is only available inside the handler function
-jQuery( document ).ready(function( $ ) {
+$(function() {
   $('footer').prepend(new Date().getFullYear() + '&nbsp;');
 });
