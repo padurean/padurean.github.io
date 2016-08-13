0c416bb417b2b8114e9ab3c4230bdde20a7ad19f
Ogg (Valentin Padurean) <valentin.padurean@bitsoftware.ro>
2016-08-13 22:06:02 +0300

    Update sample post

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 // On document ready handler
 $(function() {
   $('footer').prepend(new Date().getFullYear());
+
+  var langLabels =
+    $('div.prism-show-language > div.prism-show-language-label');
+  for (var i=0; i<langLabels.length; i++) {
+    var langLabel = $(langLabels[i]);
+    var lang = langLabel.text().toLowerCase();
+    if (lang === 'scala') {
+      langLabel.css('background-image', 'url("/img/scala-logo.svg")');
+    } else if (lang === 'javascript') {
+      // TODO OGG
+    }
+  }
+
 });
