67cc88db5d96747c4e9c023d21ec14b764e9e857
Valentin Ogg Padurean <912518+padurean@users.noreply.github.com>
2018-12-22 18:58:07 +0200

    Put the latest version from markedista with just a sample, Hello World post

---
+++ _src/_js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.post-date').each(function(index) {
+    var postDateJqElem = $(this);
+    var postMoment = moment(postDateJqElem.attr('datetime'));
+    postDateJqElem.text(postMoment.toDate().toLocaleDateString());
+    postDateJqElem.parent().find('.post-ago').text(postMoment.fromNow());
+    postDateJqElem.parent().animate( { opacity: 1 }, 1000 );
+    // OR:
+    // postDateJqElem.parent().removeClass('invisible');
+  });
+  $('#year-placeholder').text(new Date().getFullYear());
+});
