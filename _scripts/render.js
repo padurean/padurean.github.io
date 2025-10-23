9fc0811f4fbe6367175ab7f4a1e89763b40050df
Valentin Padurean (Ogg) <912518+padurean@users.noreply.github.com>
2025-10-24 01:21:15 +0300

    change border and background for giscus comments section

--- _scripts/render.js
+++ _scripts/render.js
@@ -62,9 +62,9 @@ const config = {
     linkedin: 'vpadure'
   },
   cssFiles: {
-    main: { name: 'bundle-main.min.css', version: 2 },
-    post: { name: 'bundle-post.min.css', version: 2 },
-    postWithGallery: { name: 'bundle-post-with-gallery.min.css', version: 2 }
+    main: { name: 'bundle-main.min.css', version: 3 },
+    post: { name: 'bundle-post.min.css', version: 3 },
+    postWithGallery: { name: 'bundle-post-with-gallery.min.css', version: 3 }
   },
   jsFiles: {
     main: { name: 'bundle-main.min.js', version: 1 },
